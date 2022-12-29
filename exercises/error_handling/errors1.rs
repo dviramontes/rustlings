8eefcf7fc0d84d8b41d0dec4b1e06d895a3440ff
David A. Viramontes <dviramontes@gmail.com>
2022-12-29 17:28:54 -0500

    checkpoint: 56

--- exercises/error_handling/errors1.rs
+++ exercises/error_handling/errors1.rs
@@ -8,7 +8,7 @@
 pub fn generate_nametag_text(name: String) -> Result<String, String> {
     if name.is_empty() {
         // Empty names aren't allowed.
-        Err("`name` was empty; it must be nonempty.".to_string())
+        Err("`name` was empty; it must be nonempty.".into())
     } else {
         Ok(format!("Hi! My name is {}", name))
     }
