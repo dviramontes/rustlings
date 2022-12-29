83b57f17b2a3672fdb76f2ee4bb80f56d373baa6
David A. Viramontes <dviramontes@gmail.com>
2022-12-29 12:22:23 -0500

    checkpoint: 54

--- exercises/error_handling/errors4.rs
+++ exercises/error_handling/errors4.rs
@@ -3,6 +3,8 @@
 
 // I AM NOT DONE
 
+use std::f32::consts::E;
+
 #[derive(PartialEq, Debug)]
 struct PositiveNonzeroInteger(u64);
 
@@ -15,7 +17,21 @@ enum CreationError {
 impl PositiveNonzeroInteger {
     fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
         // Hmm...? Why is this only returning an Ok value?
-        Ok(PositiveNonzeroInteger(value as u64))
+
+        // if value < 0 {
+        //     Err(CreationError::Negative)
+        // } else if value == 0 {
+        //     Err(CreationError::Zero)
+        // } else {
+        //     Ok(PositiveNonzeroInteger(value as u64))
+        // }
+
+        // as a match
+        match value {
+            num if num < 0 => Err(CreationError::Negative),
+            0 => Err(CreationError::Zero),
+            value => Ok(PositiveNonzeroInteger(value as u64))
+        }
     }
 }
 
